a34f895c149d0a6c8b8ca2a4de73939b20db4505
Willian Ribeiro <willribs29@gmail.com>
2020-11-12 19:14:13 -0300

    test: testing API dependencies, using Origamids API. Was Used forms for send informations using methods POST and GET, Authentication with Token Plugin

---
+++ src/api/Endpoint/TokenPost.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const TokenPost = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [token, setToken] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        setToken(json.token);
+        return json;
+      });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={({ target }) => setUsername(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Password"
+        value={password}
+        onChange={({ target }) => setPassword(target.value)}
+      />
+      <button>Enviar</button>
+      <h3 style={{wordBreak: "break-all"}}>{token}</h3>
+    </form>
+  );
+};
+
+export default TokenPost;
